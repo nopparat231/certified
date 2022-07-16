eb2fd6f7565457b8b044536dd733a89d8befd632
nopparat231 <23.noop@gmail.com>
2022-07-16 15:37:59 +0700

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,16 +1,12 @@
 <?php
 session_start();
 
-$url = parse_url("mysql://ba98fb408a6675:c06ed440@us-cdbr-east-05.cleardb.net/heroku_4553c8b8795a2be?reconnect=true");
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
 
-// $hostname_db = "localhost";
-// $userid_db = "root";
-// $userpwd_db = "2411";
-// $db_name = "ethnics";
+
+$hostname_db = "localhost";
+$userid_db = "root";
+$userpwd_db = "";
+$db_name = "ethnics";
 
 $conn = mysqli_connect($server, $username, $password, $db);
 mysqli_set_charset($conn, 'utf8');
